8aa2e9f21ab882abc4053d5e4300e852922eb178
Louis <Wenjin.Li@csiro.au>
2019-02-14 10:02:28 +1000

    Updates

---
+++ precache-manifest.545e8a52cbb9a31015673c9837157087.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aea48c0ed63bfd64d7fce7873ddc25e",
+    "url": "/mrlouisli/static/media/griffith_library.0aea48c0.jpg"
+  },
+  {
+    "revision": "859da6ac39d513129dd6",
+    "url": "/mrlouisli/static/css/main.2533553d.chunk.css"
+  },
+  {
+    "revision": "12930a0d6541ecc5b031",
+    "url": "/mrlouisli/static/js/runtime~main.12930a0d.js"
+  },
+  {
+    "revision": "0dabbf3619bd345fc65b",
+    "url": "/mrlouisli/static/js/2.0dabbf36.chunk.js"
+  },
+  {
+    "revision": "6b905a2a43bc8cf7016e542403e20680",
+    "url": "/mrlouisli/static/media/CSIRO_dutton_park.6b905a2a.jpeg"
+  },
+  {
+    "revision": "859da6ac39d513129dd6",
+    "url": "/mrlouisli/static/js/main.859da6ac.chunk.js"
+  },
+  {
+    "revision": "bfbd4718c07b104351843ccc54a17f04",
+    "url": "/mrlouisli/static/media/ndis_logo.bfbd4718.png"
+  },
+  {
+    "revision": "7b18989815ea2b0fb3b207c4f2742e7a",
+    "url": "/mrlouisli/static/media/background_desktop.7b189898.jpg"
+  },
+  {
+    "revision": "f9a723a3ca5e2ec23352e76ddee6c64c",
+    "url": "/mrlouisli/static/media/facebook_logo.f9a723a3.svg"
+  },
+  {
+    "revision": "0d05e7def87658dcd07824b097c866a8",
+    "url": "/mrlouisli/static/media/linkedin_logo.0d05e7de.svg"
+  },
+  {
+    "revision": "9b52262d2afc06eb61b161cb887fc1a4",
+    "url": "/mrlouisli/index.html"
+  }
+];
